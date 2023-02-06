51eb2d5483287da869dab0124241bb94e60e08f9
Carl Olsen <unstoppablecarlolsen@gmail.com>
2023-02-06 18:22:51 -0500

    progress

--- src/js/app/table-of-contents.js
+++ src/js/app/table-of-contents.js
@@ -1,21 +1,46 @@
 import OffCanvas from 'bootstrap/js/dist/offcanvas.js'
 
-const offCanvasId = 'offcanvas-toc'
-const offCanvasEl = document.getElementById(offCanvasId)
-const bsOffcanvas = new OffCanvas('#' + offCanvasId)
+const offCanvasToggleEl = document.getElementById('btn-table-of-contents')
 
+const offCanvasEl = document.getElementById('offcanvas-toc')
+const bsOffcanvas = new OffCanvas(offCanvasEl)
+const tocLockToggle = document.getElementById('toc-lock-toggle')
+let locked = false
+
+offCanvasToggleEl.addEventListener('click', function (e) {
+    e.preventDefault()
+    e.stopPropagation()
+    bsOffcanvas.show()
+})
+
+//e.preventDefault();
+//e.stopPropagation();
+//bsOffcanvas.toggle();
 document.querySelectorAll('.table-of-contents a')
 
     .forEach((el) => {
         el.addEventListener('click', (e) => {
-            e.preventDefault()
 
-            let listener = offCanvasEl.addEventListener('hidden.bs.offcanvas', event => {
-                window.location = el.getAttribute('href')
-                offCanvasEl.removeEventListener('hidden.bs.offcanvas', listener)
-            })
+            if (!locked) {
+                e.preventDefault()
 
-            bsOffcanvas.hide()
+                let listener = offCanvasEl.addEventListener('hidden.bs.offcanvas', event => {
+                    window.location = el.getAttribute('href')
+                    offCanvasEl.removeEventListener('hidden.bs.offcanvas', listener)
+                })
+                bsOffcanvas.hide()
+            }
         })
     })
 
+tocLockToggle.addEventListener('click', (e) => {
+    locked = !locked
+
+    if (locked) {
+        tocLockToggle.classList.add('locked')
+        tocLockToggle.classList.remove('unlocked')
+    } else {
+        tocLockToggle.classList.remove('locked')
+        tocLockToggle.classList.add('unlocked')
+    }
+})
