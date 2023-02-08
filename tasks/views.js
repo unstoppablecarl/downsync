035bfe25f92191b9d9201aa594784a2c4ffa2116
Carl Olsen <unstoppablecarlolsen@gmail.com>
2023-02-08 15:36:21 -0500

    progress

--- tasks/views.js
+++ tasks/views.js
@@ -2,16 +2,11 @@ import gulp from 'gulp'
 import { exec } from 'child_process'
 import util from 'util'
 import browsersync from 'browser-sync'
-import { generateSvgSprites } from './svg.js'
 
 const execPromise = util.promisify(exec)
 
-gulp.task('views', function () {
-
-    return generateSvgSprites()
-        .then(() => {
-            return execPromise('npm run build-views --color always')
-                .then(browsersync.reload)
-        })
-
+gulp.task('view-templates', function () {
+    return execPromise('npm run build-views --color always')
+        .then(browsersync.reload)
 })
+gulp.task('views', gulp.series('svg', 'view-templates'))
