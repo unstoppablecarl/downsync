6e07257f727b086930aecac4d8bf40a985aa6348
Carl Olsen <unstoppablecarlolsen@gmail.com>
2023-02-08 15:52:17 -0500

    progress

--- bin/build-pdfs.js
+++ bin/build-pdfs.js
@@ -38,6 +38,8 @@ server.listen(8080, async () => {
         return async () => {
 
             const page = await browser.newPage()
+            await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36')
+
             const content = await getPdf({
                 page,
                 url,
