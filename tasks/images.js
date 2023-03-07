39bd5983baa7f9e34563979fc75da9520fea9199
Carl Olsen <unstoppablecarlolsen@gmail.com>
2023-03-07 15:32:34 -0500

    optimize images

--- tasks/images.js
+++ tasks/images.js
@@ -1,11 +1,16 @@
 import gulp from 'gulp'
 import browsersync from 'browser-sync'
+import imagemin from 'gulp-imagemin'
+
 import { config, paths } from '../gulpfile.js'
 import plumber from 'gulp-plumber'
 
 gulp.task('images', function () {
     return gulp.src(paths.images.src)
         .pipe(plumber(config.plumber))
+        .pipe(imagemin({
+            //verbose: true
+        }))
         .pipe(gulp.dest(paths.images.dist))
         .pipe(browsersync.stream())
 })
