ab15359196f162b0a77be013557aa5b78c5c9481
Carl Olsen <unstoppablecarlolsen@gmail.com>
2023-09-15 12:00:48 -0400

    progress

--- src/views/templates-data/unit-cards-print-landscape.js
+++ src/views/templates-data/unit-cards-print-landscape.js
@@ -1,6 +1,10 @@
 import { cardsToPages, FACTION_UNITS, prepareSplitCards } from './support/page-card-data.js'
 import { COALITION_FACTION_SLUG } from '../../data/constants.js'
 
+//export const cardPages = FACTION_UNITS.flatMap((faction) => {
+//    return cardsToPages(faction.factionCards, 6)
+//})
+
 export const factionCards = FACTION_UNITS.map((faction) => {
     let UNITS = faction.factionCards
 
@@ -8,8 +12,7 @@ export const factionCards = FACTION_UNITS.map((faction) => {
         UNITS = prepareSplitCards(UNITS)
     }
 
-    faction.cardPages = cardsToPages(UNITS, 9)
+    faction.cardPages = cardsToPages(UNITS, 6)
     return faction
 })
-
 export const pageTitle = 'Unit Cards Print'
